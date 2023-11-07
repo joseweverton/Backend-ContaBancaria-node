41e6d573ef4e609ce3ad2408978806f795f7823d
jwcosta-br <jwcosta.br@gmail.com>
2023-11-07 17:30:38 -0300

    AtualidarDadosUsuarios e excluirConta

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const { autenticacaoSenha } = require("./middleware/autenticacao")
-const { listarContas, criarConta } = require("./controllers/contas")
+const { listarContas, criarConta, atualizarDadosUsuario, excluirConta } = require("./controllers/contas")
 const { validarCampos } = require("./middleware/validacao")
 
 const rotas = express()
 
 rotas.get("/contas", autenticacaoSenha, listarContas)
 rotas.post("/contas", validarCampos, criarConta )
+rotas.put("/contas/:numeroConta/usuario", validarCampos, atualizarDadosUsuario)
+rotas.delete("/contas/:numeroConta", excluirConta)
 
 module.exports = rotas
